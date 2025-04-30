151ac6865bda7d210d319e98796acc80ee679817
hotpotyy <sylviayao0402@gmail.com>
2025-04-30 13:04:25 -0400

    test testbench

--- testbench/sim_build/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilated_threads.cpp \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilatedos.h \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilated_threads.h \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilated.h \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilated_config.h \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilated_types.h \
- /afs/ece.cmu.edu/class/ece224/cad_tools/oss-cad-suite/share/verilator/include/verilated_funcs.h
